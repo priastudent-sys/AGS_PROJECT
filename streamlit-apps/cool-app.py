ae35dded5f9f34db79095b8090dd0e5324875a42
priastudent-sys <pria.student@altaglobalsch.com>
2026-04-15 12:29:51 +0000

    creating first streamlit app

---
+++ streamlit-apps/cool-app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Cool App")
+
+st.write("welcome to this world. lets have some fun!!!!!")
+
+st.header("lets do some cool stuff here")
+st.subheader("like making code for gundam.")
+
+st.caption("lets have fun with streamlit and python")
+
+if st.button("click me"):
+    st.write("you clicked the button")
+    st.balloons()
+    st.image("https://www.imdb.com/title/tt0159184/")
